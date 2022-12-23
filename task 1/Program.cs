8b07112b574fe05ec79ae03fdf9bcc2150f55e21
Alexande <79607025659@yandex.ru>
2022-12-23 18:15:27 +0300

    111

--- task 1/Program.cs
+++ task 1/Program.cs
@@ -4,3 +4,77 @@
 string number = Console.ReadLine();
 
     Console.WriteLine("второе число, трехзначного числа: " +number[1]);
+
+
+
+
+Console.WriteLine("введите трехзначное число: ");
+
+string number = Console.ReadLine();
+if (string number > 99)
+
+    Console.WriteLine("третья цифра, трехзначного числа: " +number[2]); 
+
+
+
+Console.WriteLine("введите число обозгочающее день недели");
+int day = Convert.ToInt32(Console.ReadLine());
+switch(day) 
+    {
+        case 1:
+        Console.WriteLine("рабочий");
+        break;
+
+        case 2:
+        Console.WriteLine("рабочий");
+        break;
+
+        case 3:
+        Console.WriteLine("рабочий");
+        break;
+
+        case 4:
+        Console.WriteLine("рабочий");
+        break;
+
+        case 5:
+        Console.WriteLine("рабочий");
+        break;
+
+        case 6:
+        Console.WriteLine("выходной");
+        break;
+
+        case 7:
+        Console.WriteLine("выходной");
+        break;
+
+        default:
+        Console.WriteLine("нет дня");
+        break;
+    }
+
+
+
+
+
+Console.WriteLine("введите число 1");
+int number1 =Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число 2");
+int number2 =Convert.ToInt32(Console.ReadLine());
+int quadro1 =  number1 * number1;
+int quadro2 =  number2 * number2;
+
+
+
+
+/* if (quadro1 ==  number1 * number1 || quadro2 ==  number2 * number2)*/   if  ( quadro1 == number2 || quadro2 == number1)
+
+    Console.WriteLine("ЯВЛЯЕТСЯ " );
+else
+    Console.WriteLine("не ЯВЛЯЕТСЯ " );
+
+
+
+
+
