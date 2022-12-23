1421651b829ffe8c761d23bcc3cfe663e0355d4e
Alexande <79607025659@yandex.ru>
2022-12-23 15:07:56 +0300

    1

---
+++ task 1/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("введите трехзначное число: ");
+//int number =Convert.ToInt32(Console.ReadLine());
+    
+string number = Console.ReadLine();
+
+    Console.WriteLine("второе число, трехзначного числа: " +number[1]);
